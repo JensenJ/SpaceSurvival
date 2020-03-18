8d6fc2ae44a45281ac3d7ba42581020f57d65472
JensenJ <jensenbradley@icloud.com>
2020-03-18 16:43:19 +0000

    Revert "DOTS Chunk Perlin Noise"
    This reverts commit 6dbb91c3b79716966bbac25402502f2e88c8f926.

--- Assets/Scripts/MapGeneration/Chunk.cs
+++ Assets/Scripts/MapGeneration/Chunk.cs
@@ -1,10 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Jobs;
-using Unity.Burst;
-using Unity.Mathematics;
-using Unity.Collections;
 
 //A class to generate a mesh from cube data
 public class Chunk
@@ -22,17 +18,20 @@ public class Chunk
     //Terrain variables
     float terrainSurface = 0.5f;
 
-    int width { get { return MarchingData.ChunkWidth; } }
-    int height { get { return MarchingData.ChunkHeight; } }
-    float terrainHeight { get { return MarchingData.terrainSurface; } }
-
+    int width { get { return MarchingData.ChunkWidth;}}
+    int height {get {return MarchingData.ChunkHeight;}}
+    float terrainHeight {get {return MarchingData.terrainSurface;}}
+    
     float[,,] terrainMap;
 
     public float frequency;
 
+    bool smoothTerrain = false;
+    bool flatShading = true;
+
     public Vector3Int chunkPosition;
 
-    public Chunk(Vector3Int position, float _frequency)
+    public Chunk(Vector3Int position, float _frequency, bool _smoothTerrain)
     {
         chunkObject = new GameObject();
         chunkPosition = position;
@@ -40,6 +39,8 @@ public class Chunk
         chunkObject.name = string.Format("Chunk {0}, {1}", position.x, position.z);
 
         frequency = _frequency;
+        smoothTerrain = _smoothTerrain;
+        flatShading = !_smoothTerrain;
 
         //Create arrays and get components
         meshFilter = chunkObject.AddComponent<MeshFilter>();
@@ -49,91 +50,8 @@ public class Chunk
         chunkObject.transform.tag = "Terrain";
 
         terrainMap = new float[width + 1, height + 1, width + 1];
-        //CreateMeshData();
-
-        ClearMeshData();
-        
-        BuildMesh();
+        CreateMeshData();
     }
-
-    void GenerateChunkData()
-    {
-
-        //NativeList<Vector3> jobVertices = new NativeList<Vector3>(Allocator.TempJob);
-        //NativeList<int> jobTriangles = new NativeList<int>(Allocator.TempJob);
-
-        //GenerateChunkDataJob chunkJob = new GenerateChunkDataJob()
-        //{
-        //    width = width,
-        //    height = height,
-        //    frequency = frequency,
-        //    surfaceLevel = terrainSurface,
-        //    chunkPosition = chunkPosition,
-        //    vertices = jobVertices,
-        //    triangles = jobTriangles,
-        //};
-
-        //JobHandle chunkHandle = chunkJob.Schedule();
-        //chunkHandle.Complete();
-
-        //for (int i = 0; i < jobVertices.Length; i++)
-        //{
-        //    vertices.Add(jobVertices[i]);
-        //}
-
-        //for (int i = 0; i < jobTriangles.Length; i++)
-        //{
-        //    triangles.Add(jobTriangles[i]);
-        //}
-
-        //jobVertices.Dispose();
-        //jobTriangles.Dispose();
-
-
-    }
-
-    //Function to return a scheduled job and the heights array for the mesh data generation.
-    public List<JobHandle> ScheduleChunkHeightDataGeneration(int chunkSize, int chunkHeight, float frequency, float baseTerrainHeight, float terrainHeightRange, out List<NativeList<float>> heights)
-    {
-        //Heights list initialiser
-        heights = new List<NativeList<float>>();
-
-        //height sub-list initialisation
-        for (int i = 0; i < chunkHeight + 1; i++)
-        {
-            heights.Add(new NativeList<float>(Allocator.TempJob));
-        }
-
-        //List for the jobhandles
-        List<JobHandle> chunkNoiseGenerationHandles = new List<JobHandle>();
-
-        //For every layer
-        for (int i = 0; i < chunkHeight + 1; i++)
-        {
-            //Job creation
-            ChunkNoiseGenerationJob chunkNoiseGenerationJob = new ChunkNoiseGenerationJob()
-            {
-                chunkHeight = chunkHeight,
-                chunkSize = chunkSize,
-                y = i,
-                frequency = frequency,
-                baseTerrainHeight = baseTerrainHeight,
-                terrainHeightRange = terrainHeightRange,
-                result = heights[i],
-            };
-
-            //Add scheduled job to handle array
-            chunkNoiseGenerationHandles.Add(chunkNoiseGenerationJob.Schedule());
-
-        }
-        //Return scheduled job array, ready for completion
-        return chunkNoiseGenerationHandles;
-    }
-
-    //public JobHandle ScheduleChunkMeshDataGeneration()
-    //{
-
-    //}
 
     //Function to populate the terrain map array
     void PopulateTerrainMap()
@@ -147,10 +65,12 @@ public class Chunk
                 //For every point on z axis
                 for (int z = 0; z < width + 1; z++)
                 {
+
+
                     //Height generation using a noise function
                     float thisHeight = MarchingData.GetTerrainHeight(x + chunkPosition.x, z + chunkPosition.z, frequency);
 
-                    if (x > 5 && x < 11 && z > 5 && z < 11)
+                    if(x > 5 && x < 11 && z > 5 && z < 11)
                     {
                         thisHeight = MarchingData.BaseTerrainHeight;
                     }
@@ -191,7 +111,7 @@ public class Chunk
         //For every vertex in cube data
         for (int i = 0; i < 8; i++)
         {
-            if (cube[i] > terrainSurface)
+            if(cube[i] > terrainSurface)
             {
                 //Bit shift, setting index at a byte level based on the cube vertex index
                 configurationIndex |= 1 << i;
@@ -205,8 +125,6 @@ public class Chunk
     //Function to march the cube
     void MarchCube(Vector3Int position)
     {
-        float time = Time.time;
-
         //Create cube array
         float[] cube = new float[8];
         //For every vertex in cube
@@ -245,10 +163,45 @@ public class Chunk
                 Vector3 vert1 = position + MarchingData.vertexTable[MarchingData.edgeTable[index, 0]];
                 Vector3 vert2 = position + MarchingData.vertexTable[MarchingData.edgeTable[index, 1]];
 
-                //Get vertex midpoint position from edge
-                Vector3 vertPosition = (vert1 + vert2) / 2f;
+                Vector3 vertPosition;
+                if (smoothTerrain)
+                {
+                    //Get the terrain values at either end of the edge
+                    float vert1Sample = cube[MarchingData.edgeTable[index, 0]];
+                    float vert2Sample = cube[MarchingData.edgeTable[index, 1]];
 
-                triangles.Add(VertForIndex(vertPosition));
+                    //Calculate the difference between the two terrain values. 
+                    float difference = vert2Sample - vert1Sample;
+                    //If difference is 0, then the terrain passes through middle.
+                    if(difference == 0)
+                    {
+                        difference = terrainSurface;
+                    }
+                    else
+                    {
+                        difference = (terrainSurface - vert1Sample) / difference;
+                    }
+                    //Calculate the point along the edge that the terrain passes through.
+                    vertPosition = vert1 + ((vert2 - vert1) * difference);
+                }
+                else
+                {
+                    //Get vertex midpoint position from edge
+                    vertPosition = (vert1 + vert2) / 2f;
+
+                }
+
+                //Add generated data to mesh data lists
+                if (flatShading)
+                {
+                    vertices.Add(vertPosition);
+                    triangles.Add(vertices.Count - 1);
+
+                }
+                else
+                {
+                    triangles.Add(VertForIndex(vertPosition));
+                }
                 edgeIndex++;
             }
         }
@@ -261,9 +214,6 @@ public class Chunk
                 uvs.Add(new Vector2((float)x / width, (float)z / width));
             }
         }
-
-
-        Debug.Log((Time.time - time) * 1000 + "ms");
     }
 
     public void PlaceTerrain(Vector3 pos)
@@ -297,7 +247,7 @@ public class Chunk
         for (int i = 0; i < vertices.Count; i++)
         {
             //If a vert is found that matches passed in vert, return this index.
-            if (vertices[i] == vert)
+            if(vertices[i] == vert)
             {
                 return i;
             }
@@ -326,30 +276,5 @@ public class Chunk
         mesh.name = "Terrain";
         meshFilter.mesh = mesh;
         meshCollider.sharedMesh = mesh;
-    }
-}
-
-//A job to generate simplex / perlin noise efficiently
-[BurstCompile]
-public struct ChunkNoiseGenerationJob : IJob
-{
-    public float chunkSize;
-    public float chunkHeight;
-    public float y;
-    public float frequency;
-    public float baseTerrainHeight;
-    public float terrainHeightRange;
-    public NativeList<float> result;
-
-    public void Execute()
-    {
-        for (int x = 0; x < chunkSize + 1; x++)
-        {
-            for (int i = 0, z = 0; z < chunkSize; z++)
-            {
-                result.Add(y - terrainHeightRange * noise.snoise(new float2(x * frequency / 1000, z * frequency / 1000)) + baseTerrainHeight);
-                i++;
-            }
-        }
     }
 }
--- Assets/Scripts/MapGeneration/MapGenerator.cs
+++ Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -1,14 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Jobs;
-using Unity.Collections;
-using Unity.Mathematics;
 
 public class MapGenerator : MonoBehaviour
 {
     [SerializeField] int worldSizeInChunks = 10;
     [SerializeField] float frequency = 85;
+    [SerializeField] bool smoothTerrain = true;
 
     Dictionary<Vector3Int, Chunk> chunks = new Dictionary<Vector3Int, Chunk>();
 
@@ -18,48 +16,16 @@ public class MapGenerator : MonoBehaviour
         Generate();
     }
 
-    private void Update()
-    {
-        GenerateChunkHeights();
-    }
-
     void Generate()
     {
-        //Position setting for chunks
         for (int x = 0; x < worldSizeInChunks; x++)
         {
             for (int z = 0; z < worldSizeInChunks; z++)
             {
-                //Array creation
                 Vector3Int chunkPos = new Vector3Int(x * MarchingData.ChunkWidth, 0, z * MarchingData.ChunkWidth);
-                chunks.Add(chunkPos, new Chunk(chunkPos, frequency));
+                chunks.Add(chunkPos, new Chunk(chunkPos, frequency, smoothTerrain));
                 chunks[chunkPos].chunkObject.transform.SetParent(transform);
             }
-        }
-
-        GenerateChunkHeights();
-    }
-
-    void GenerateChunkHeights()
-    {
-        //Create list for height gen jobs
-        NativeList<JobHandle> heightGenJobs = new NativeList<JobHandle>(Allocator.Temp);
-        List<NativeList<float>> allHeights = new List<NativeList<float>>();
-
-        List<JobHandle> tempHandles = chunks[new Vector3Int(0, 0, 0)].ScheduleChunkHeightDataGeneration(MarchingData.ChunkWidth, MarchingData.ChunkHeight, frequency, MarchingData.BaseTerrainHeight,
-            MarchingData.TerrainHeightRange, out allHeights);
-
-        for (int i = 0; i < tempHandles.Count; i++)
-        {
-            heightGenJobs.Add(tempHandles[i]);
-        }
-
-        JobHandle.CompleteAll(heightGenJobs);
-        heightGenJobs.Dispose();
-
-        for (int i = 0; i < allHeights.Count; i++)
-        {
-            allHeights[i].Dispose();
         }
     }
 }
